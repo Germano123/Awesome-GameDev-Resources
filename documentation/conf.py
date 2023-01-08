134a7582e2f2e153c2dd4fcd30a257cd99395214
tolstenko <tolstenko@users.noreply.github.com>
2023-01-08 23:11:12 +0000

    deploy: ae78e90901cf30732f11959eac7e35a2b3a3db62

---
+++ documentation/conf.py
@@ -0,0 +1,31 @@
+DOXYFILE = 'Doxyfile'
+
+LINKS_NAVBAR1 = [
+    ("<a href=\"/\">Courses</a>", [(None ,'md_intro_README'), (None, 'md_portfolio_README'), (None, 'md_artificialintelligence_README')]),
+    # ("Namespaces", 'namespaces', [])
+]
+
+# Add your own navbar links using the code below.
+# To find the valid link names, you can inspect the URL of a generated documentation site.
+
+# LINKS_NAVBAR1 = [
+#     (None, 'pages', [(None, 'about')]),
+#     (None, 'namespaces', [(None, 'namespaceconcurrency')]),
+# ]
+#
+# LINKS_NAVBAR2 = [
+#     (None, 'annotated', [(None, 'classconcurrency_1_1_concurrency')]),
+#     (None, 'files', [(None, 'concurrency_8h')]),
+# ]
+
+FINE_PRINT = """Awesome Cpp Course. Created by <a href="https://linktree.com/tolstenko">Alexandre Tolstenko</a> and <a href="https://github.com/InfiniBrains/Introduction-to-Game-Programming-With-CPP/graphs/contributors">Others</a>. 2023. All rights reserved."""
+
+HTML_HEADER = """<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-76LZTVL113"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-76LZTVL113');
+</script>"""
